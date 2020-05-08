40eb4cf73f876ae832415dd8d1ec9cde977cf39f
Nicolas A Perez <anicolaspp@gmail.com>
2020-05-08 12:23:09 -0400

    Update RandomRowGenerator.java

--- contrib/storage-np/src/main/java/org/apache/drill/exec/store/np/read/RandomRowGenerator.java
+++ contrib/storage-np/src/main/java/org/apache/drill/exec/store/np/read/RandomRowGenerator.java
@@ -5,30 +5,41 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.ojai.Document;
 
 import java.util.Iterator;
+import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Stream;
 
+/**
+ * Generates random rows with a fixed schema.
+ *
+ * The resulting batch is infinite or we can apply an option limit to it.
+ */
 public class RandomRowGenerator implements RowGenerator<Row> {
 
-    ObjectMapper mapper = new ObjectMapper();
-    Random random = new Random();
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final Random random = new Random();
 
-    private Row genNextRandomRow() {
-        return new Row(
-                random.nextInt(10),
-                random.nextInt(100),
-                String.valueOf(random.nextLong()));
+    private final Optional<Integer> limit;
+
+    public RandomRowGenerator() {
+        this(0);
+    }
+
+    public RandomRowGenerator(Integer limit) {
+        if (limit > 0) {
+            this.limit = Optional.of(limit);
+        } else {
+            this.limit = Optional.empty();
+        }
     }
 
     @Override
     public Iterator<Row> getRows() {
-        return Stream
-                .generate(this::genNextRandomRow)
-                .limit(10)
-                .iterator();
+        Stream<Row> rows = Stream.generate(this::genNextRandomRow);
+
+        return applyLimit(rows).iterator();
     }
 
     @Override
@@ -40,6 +51,17 @@ public class RandomRowGenerator implements RowGenerator<Row> {
 
             return new byte[0];
         }
+    }
+
+    private Row genNextRandomRow() {
+        return new Row(
+                random.nextInt(10),
+                random.nextInt(100),
+                String.valueOf(random.nextLong()));
+    }
+
+    private Stream<Row> applyLimit(Stream<Row> rows) {
+        return limit.map(rows::limit).orElse(rows);
     }
 }
 
